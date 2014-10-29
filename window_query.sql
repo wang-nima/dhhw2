9c81d20bb3876ec53d45468f06daa17bf6d90321
shawn <jak47042007@163.com>
2014-10-28 22:11:49 -0700

    haha

--- window_query.sql
+++
@@ -1,8 +0,0 @@
-select s.student_id
-from students s
-where SDO_INSIDE(
-  s.shape,
-  SDO_GEOMETRY(2003,NULL,NULL,
-    SDO_ELEM_INFO_ARRAY(1,1003,3),
-    SDO_ORDINATE_ARRAY(100,100,300,300))
-) ='TRUE';
